8d6dbb0f8fc0c0473ca5ce47c34e61d557d7c926
Guilherme Aramizo Ribeiro <Guilherme Aramizo Ribeiro>
2016-09-19 13:47:43 -0400

    Better display on sample code

--- test_matleap_image.m
+++ test_matleap_image.m
@@ -1,19 +1,25 @@
-N = 1000;
+fps = 50;
+T = 10;
+N = T * fps;
+
 idxs = zeros(N, 1);
 times = zeros(N, 1);
 
-[img1, img2, idx] = matleap_image;
-h = imshow([img1 img2]);
+idx = -1;
+while idx == -1
+    [img1, img2, idx] = matleap_image;
+end
+h = imshow(permute([img1 img2], [2 1]));
 
 t0 = tic;
 for k = 1 : N
     tic
     [img1, img2, idx] = matleap_image;
-    set(h, 'CData', [img1 img2])
+    set(h, 'CData', permute([img1 img2], [2 1]))
     
     idxs(k) = idx;
     times(k) = toc(t0);
     
-    pause(1/90 - toc)
+    pause(1/fps - toc)
 end
 toc(t0)
